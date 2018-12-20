18210bca8a9065798b455cbac30c7dcaebb440bf
Dominic Magnifico <dominicmagnifico@gmail.com>
2018-12-20 11:22:05 -0700

    Connect apollo to yoga.

---
+++ frontend/lib/withData.js
@@ -0,0 +1,20 @@
+import withApollo from 'next-with-apollo';
+import ApolloClient from 'apollo-boost';
+import { endpoint } from '../config';
+
+function createClient({ headers }) {
+  return new ApolloClient({
+    // TODO :: Ensure that the production env endpoint changes when deploying.
+    uri: process.env.NODE_ENV === 'development' ? endpoint : endpoint,
+    request: operation => {
+      operation.setContext({
+        fetchOptions: {
+          credentials: 'include',
+        },
+        headers,
+      });
+    },
+  });
+}
+
+export default withApollo(createClient);
